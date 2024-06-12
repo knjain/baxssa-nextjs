39c751a25ea970cdd55b2f43748774ce138a9a7f
knjain <knjain1999@gmail.com>
2024-06-13 03:59:05 +0530

    wdqw

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -37,3 +37,57 @@ const UsersPage = async () => {
 };
 
 export default UsersPage;
+
+
+// import axios from "axios";
+// import { BASE_API_URL } from "@/constants/constants";
+// import React from "react";
+
+// // Fetch data on the server
+// async function fetchData() {
+//   try {
+//     const response = await axios.get(`${BASE_API_URL}/users`, {
+//       headers: {
+//         "Cache-Control": "no-store",
+//       },
+//     });
+//     return response.data.data;
+//   } catch (error: any) {
+//     if (error.response) {
+
+//       throw new Error(`Request failed with status ${error.response.status}`);
+//     } else if (error.request) {
+//       // The request was made but no response was received
+//       throw new Error("No response received from server");
+//     } else {
+//       // Something happened in setting up the request that triggered an Error
+//       throw new Error(`Error setting up the request: ${error.message}`);
+//     }
+//   }
+// }
+
+// const UsersPage = async () => {
+//   const users = await fetchData();
+
+//   interface User {
+//     userId: string;
+//     fullName: string;
+//     email: string;
+//     phoneNumber: string;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       <ul>
+//         {users.map((user: User) => (
+//           <li key={user.userId}>
+//             {user.fullName} - {user.email} - {user.phoneNumber}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default UsersPage;
