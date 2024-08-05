67ed6e1c83a71ea280f1e2bf652ab247595ba828
knjain1999 <ask@walker-technology.com>
2024-08-05 12:38:40 +0530

    clean up

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,11 @@ import { getToken } from "next-auth/jwt";
 export { default } from "next-auth/middleware";
 
 export async function middleware(request: NextRequest) {
-  console.log("asdasadsf")
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET,
   });
   const url = request.nextUrl.clone();
-  // If the user is authenticated and tries to access sign-in, sign-up, or the root page, redirect to admin dashboard
-  console.log(url)
   if (
     token &&
     (url.pathname === "/admin/sign-in" ||
@@ -21,7 +18,6 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
-  // If the user is not authenticated and tries to access any dashboard route or the root page, redirect to admin sign-in
   if (
     !token &&
     (url.pathname.startsWith("/admin/dashboard")
